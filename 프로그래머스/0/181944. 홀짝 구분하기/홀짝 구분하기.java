be6d4a252b7f314c1d894eb70578a5347b207d99
sllip <heu0303@naver.com>
2026-03-21 11:39:41 +0900

    [level 0] Title: 홀짝 구분하기, Time: 209.86 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -5,10 +5,7 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        if (n % 2 == 0) {
-            System.out.println(n + " is even");
-        } else {
-            System.out.println(n + " is odd");
-        }
+        System.out.print(n + " is ");
+        System.out.println(n % 2 == 0 ? "even" : "odd");
     }
 }
