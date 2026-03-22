346c561fecc3a8a363b5f70f03bc22c45ef4ecc5
sllip <heu0303@naver.com>
2026-03-22 21:12:07 +0900

    [level 0] Title: 이어 붙인 수, Time: 12.46 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -7,9 +7,9 @@ class Solution {
         
         for (int i : num_list) {
             if (i % 2 == 0) {
-                even += i;
+                even += i + "";
             } else {
-                odd += i;
+                odd += i + "";
             }
         }
         
