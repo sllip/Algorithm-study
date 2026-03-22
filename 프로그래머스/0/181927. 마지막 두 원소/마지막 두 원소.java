fd45516142e72eb306a8369819e60e133100eb21
sllip <heu0303@naver.com>
2026-03-23 03:06:22 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,17 +2,17 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length + 1];
         
-        int idx = num_list.length - 1;
-        int last = num_list[idx];
-        int prev = num_list[idx - 1];
+        int idx = num_list.length;
+        int last = num_list[idx-1];
+        int prev = num_list[idx-2];
         
         int a = (last > prev) ? (last - prev) : (last * 2);
         
-        for (int i = 0; i < num_list.length; i++) {
+        for (int i = 0; i < idx; i++) {
             answer[i] = num_list[i];
         }
         
-        answer[num_list.length] = a;
+        answer[idx] = a;
         
         return answer;
     }
