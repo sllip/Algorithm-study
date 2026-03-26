fa5d949bc29ecca9111c6d3b708ba1e1ebfffcb1
sllip <heu0303@naver.com>
2026-03-27 01:11:48 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,16 +1,9 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int[end_num - start_num + 1];
-        ArrayList<Integer> result = new ArrayList<>();
         
-        for (int i = start_num; i <= end_num; i++) {
-            result.add(i);
-        }
-        
-        for (int j = 0; j < answer.length; j++) {
-            answer[j] = result.get(j);
+        for (int i = 0; i <= end_num - start_num; i++) {
+            answer[i] = start_num + i;
         }
         
         return answer;
