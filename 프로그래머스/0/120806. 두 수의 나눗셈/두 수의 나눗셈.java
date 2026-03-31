7b8fd06a79bf1e7a8410be694cf757b3e75f9ce9
sllip <heu0303@naver.com>
2026-03-31 20:17:19 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = (int)((num1 / (double)num2) * 1000);
-        return answer;
+        return (int)((num1 / (double)num2) * 1000);
     }
 }
