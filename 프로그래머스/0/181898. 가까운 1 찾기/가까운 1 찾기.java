2810b437630026a99428321ad947c4f964fe21dc
sllip <heu0303@naver.com>
2026-06-23 20:25:36 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int[] arr, int idx) {
-        int answer = 0;
+        int answer = -1;
         
         for (int i = idx; i < arr.length; i++) {
-            if (arr[i] == 0 && i == arr.length-1) {
-                answer = -1;
-            } else if (arr[i] == 1) {
+            if (arr[i] == 1) {
                 answer = i;
                 break;
             }
