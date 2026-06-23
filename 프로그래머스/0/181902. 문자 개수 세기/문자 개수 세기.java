707be4642c73c9ffbb342ac7d10c51f533957c8a
sllip <heu0303@naver.com>
2026-06-23 13:27:58 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -2,22 +2,13 @@ class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
         
-        for (int i = 0; i < 26; i++) {
-            answer[i] = 65+i;
-        }
-        
-        for (int i = 26; i < answer.length; i++) {
-            answer[i] = 71+i;
-        }
-        
-        for (int i = 0; i < 52; i++) {
-            int count = 0;
-            for (int j = 0; j < my_string.length(); j++) {
-                if ((int)my_string.charAt(j) == answer[i]) {
-                    count++;
-                }
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (c < 'a') {
+                answer[c-65]++;
+            } else {
+                answer[c-71]++;
             }
-            answer[i] = count;
         }
         
         return answer;
