b2baa139c90e4c22a721b25925272a58514a192a
sllip <heu0303@naver.com>
2026-06-23 19:51:27 +0900

    [level 0] Title: 글자 지우기, Time: 0.85 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -3,19 +3,15 @@ import java.util.Arrays;
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
+
+        String[] str = my_string.split("");
         
-        int j = 0;
-        Arrays.sort(indices);
-        
-        for (int i = 0; i < my_string.length(); i++) {
-            if (i == indices[j]) {
-                if ((j+1) != indices.length) {
-                    j++;
-                }
-                continue;
-            } else {
-                answer += String.valueOf(my_string.charAt(i));
-            }
+        for (int i = 0; i < indices.length; i++) {
+            str[indices[i]] = "";
+        }
+
+        for (String s : str) {
+            answer += s;
         }
         
         return answer;
