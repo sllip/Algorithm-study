37d8dff393309aab516986b3c9bd276739a4229a
sllip <heu0303@naver.com>
2026-06-25 17:41:57 +0900

    [level 0] Title: 배열 조각하기, Time: 0.67 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = {};
+        
+        for (int i = 0; i < query.length; i++) {
+            if (i%2 == 0) {
+                arr = Arrays.copyOf(arr, query[i]+1);
+            } else {
+                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+            }
+        }
+        
+        answer = Arrays.copyOf(arr, arr.length);
+        
+        return answer;
+    }
+}
