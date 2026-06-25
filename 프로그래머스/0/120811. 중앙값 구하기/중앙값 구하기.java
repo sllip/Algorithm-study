f5db477fd364f66c75e452a983bc3734fc3c32c5
sllip <heu0303@naver.com>
2026-06-25 21:20:32 +0900

    [level 0] Title: 중앙값 구하기, Time: 2.29 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        int idx = array.length / 2;
+        Arrays.sort(array);
+        
+        answer = array[idx];
+        
+        return answer;
+    }
+}
