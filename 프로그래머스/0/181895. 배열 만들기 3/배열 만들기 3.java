ef64ed570ead515cc191c54f1b03019478bca5d2
sllip <heu0303@naver.com>
2026-06-25 16:50:24 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.14 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,26 +1,21 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        ArrayList<Integer> result = new ArrayList<>();
-        
         int a1 = intervals[0][0];
         int b1 = intervals[0][1];
         int a2 = intervals[1][0];
         int b2 = intervals[1][1];
         
+        int[] answer = new int[(b1-a1+1) + (b2-a2+1)];
+        
+        int j = 0;
         for (int i = a1; i <= b1; i++) {
-            result.add(arr[i]);
+            answer[j] = arr[i];
+            j++;
         }
         
         for (int i = a2; i <= b2; i++) {
-            result.add(arr[i]);
-        }
-        
-        int[] answer = new int[result.size()];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = result.get(i);
+            answer[j] = arr[i];
+            j++;
         }
         
         return answer;
