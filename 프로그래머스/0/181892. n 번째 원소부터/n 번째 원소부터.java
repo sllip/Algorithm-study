fab8973be85f580585dbb5105949bf715ec1eaed
sllip <heu0303@naver.com>
2026-06-25 19:36:30 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -2,9 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = {};
-        
-        answer = Arrays.copyOfRange(num_list, n-1, num_list.length);
+        int[] answer = Arrays.copyOfRange(num_list, n-1, num_list.length);
         
         return answer;
     }
