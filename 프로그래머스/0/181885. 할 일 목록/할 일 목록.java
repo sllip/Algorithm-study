5045b142da975699714f9eae27d9e50742cdc0a8
sllip <heu0303@naver.com>
2026-06-26 14:26:29 +0900

    [level 0] Title: 할 일 목록, Time: 3.85 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,21 +1,11 @@
-import java.util.ArrayList;
-
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        ArrayList<String> result = new ArrayList<>();
+        String result = "";
         
-        for (int i = 0; i < todo_list.length; i++) {
-            if (finished[i] == false) {
-                result.add(todo_list[i]);
-            }
+        for (int i = 0; i < finished.length; i++) {
+            result = (finished[i] == false) ? result+todo_list[i]+"," : result;
         }
         
-        String[] answer = new String[result.size()];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = result.get(i);
-        }
-        
-        return answer;
+        return result.split(",");
     }
 }
