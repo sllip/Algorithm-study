47f7f40425f04f0b9f4d2b4c428f6f0623d524a0
sllip <heu0303@naver.com>
2026-06-26 21:26:33 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n + 1) / 2];
+        
+        int num = 1;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            answer[i] = num;
+            num += 2;
+        }
+        
+        return answer;
+    }
+}
