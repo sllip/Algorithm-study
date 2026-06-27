cac7a6641af053e1393f1c119cf777123b7ee4a8
sllip <heu0303@naver.com>
2026-06-27 14:56:30 +0900

    [level 0] Title: A 강조하기, Time: 0.52 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,6 +1,15 @@
 class Solution {
     public String solution(String myString) {
-        String answer = myString.toLowerCase().replace('a', 'A');
+        String answer = "";
+        String[] result = myString.toLowerCase().split("");
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i].equals("a")) {
+                result[i] = result[i].toUpperCase();
+            }
+            answer += result[i];
+        }
+
         return answer;
     }
 }
