c1d53c1d8b0afd05116622e2b7307263f2d93f69
sllip <heu0303@naver.com>
2026-06-28 12:57:17 +0900

    [level 0] Title: 세 개의 구분자, Time: 21.57 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,21 +1,10 @@
 class Solution {
     public String[] solution(String myStr) {
-        myStr = myStr.replace("a", " ").replace("b", " ").replace("c", " ");
         
-        while (myStr.contains("  ")) {
-            myStr = myStr.replace("  ", " ");
-        }
+        myStr = myStr.replaceAll("[abc]+", ",");
+        myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
+        myStr = myStr == "" ? "EMPTY" : myStr;
         
-        if (myStr.startsWith(" ")) {
-            myStr = myStr.substring(1, myStr.length());
-        }
-        
-        String[] answer = myStr.split(" ");
-        
-        if (answer[0].equals("")) {
-            answer[0] = "EMPTY";
-        }
-        
-        return answer;
+        return myStr.split(",");
     }
 }
