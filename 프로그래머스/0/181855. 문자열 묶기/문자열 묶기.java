ecbd2b45d73536fb399bab51104355f4795d48bc
sllip <heu0303@naver.com>
2026-06-29 21:37:53 +0900

    [level 0] Title: 문자열 묶기, Time: 5.90 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int answer = 0;
+        int[] str_len = new int[30];
+        
+        for (int i = 0; i < strArr.length; i++) {
+            str_len[strArr[i].length()-1]++;
+        }
+        
+        for (int i = 0; i < str_len.length; i++) {
+            answer = str_len[i] > answer ? str_len[i] : answer;
+        }
+        
+        return answer;
+    }
+}
