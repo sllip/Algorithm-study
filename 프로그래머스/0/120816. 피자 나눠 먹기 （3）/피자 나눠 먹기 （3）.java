496df502addf74cd9cb766d71081cc373a431965
sllip <heu0303@naver.com>
2026-07-01 15:42:58 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 1;
+        
+        while (true) {
+            if (slice * answer >= n) {
+                break;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
