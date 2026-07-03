fc0e23ca0225db99e763a2396f81063e90d30ad6
sllip <heu0303@naver.com>
2026-07-03 17:24:16 +0900

    [level 0] Title: 두 수의 합, Time: 657.12 ms, Memory: 381 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,12 +1,42 @@
-import java.math.BigInteger;
-
 class Solution {
     public String solution(String a, String b) {
+        String answer = "";
         
-        BigInteger num1 = new BigInteger(a);
-        BigInteger num2 = new BigInteger(b);
+        int a_len = a.length();
+        int b_len = b.length();
+        int[] result = new int[Math.max(a_len, b_len)];
         
-        String answer = String.valueOf(num1.add(num2));
+        String[] arr1 = a.split("");
+        String[] arr2 = b.split("");
+        
+        int j = 1;
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = Integer.parseInt(arr1[a_len-j]);
+            j++;
+            if (a_len-j < 0) {
+                break;
+            }
+        }
+        
+        int k = 1;
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] += Integer.parseInt(arr2[b_len-k]);
+            k++;
+            if (b_len-k < 0) {
+                break;
+            }
+        }
+        
+        for (int i = result.length - 1; i > 0; i--) {
+            if (result[i] >= 10) {
+                result[i] -= 10;
+                result[i-1]++;
+            }
+        }
+        
+        for (int n : result) {
+            answer += String.valueOf(n);
+        }
         
         return answer;
     }
