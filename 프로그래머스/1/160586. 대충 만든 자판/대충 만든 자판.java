e68aa391cbdccdaf9f8ee7cbdc13235d2309bfd6
sllip <heu0303@naver.com>
2026-07-03 21:56:28 +0900

    [level 1] Title: 대충 만든 자판, Time: 10.28 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        int min = 100;
+        int idx = 0, click = 0;
+        boolean flag = false;
+        
+        for (int i = 0; i < targets.length; i++) {
+            for (int j = 0; j < targets[i].length(); j++) {
+                for (int k = 0; k < keymap.length; k++) {
+                    idx = keymap[k].indexOf(String.valueOf(targets[i].charAt(j)));
+                    if (idx == -1) {
+                        if ((min == 100) && (k == keymap.length-1)) {
+                            flag = true;
+                            break;
+                        } else {
+                            continue;
+                        }
+                    } else if (idx < min) {
+                        min = idx;
+                    }
+                }
+                
+                if (flag && (idx == -1)) {
+                    click = -1;
+                    flag = false;
+                    break;
+                }
+                click += (min+1);
+                min = 100;
+            }
+            
+            answer[i] = click;
+            click = 0;
+        }
+        
+        return answer;
+    }
+}
