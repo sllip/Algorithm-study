2b5c0410f9fb2d924493b2335fbbf7999b8a0a20
sllip <heu0303@naver.com>
2026-07-04 19:21:10 +0900

    [level 0] Title: 커피 심부름, Time: 0.93 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for (String menu : order) {
+            answer += menu.contains("latte") ? 5000 : 4500;
+        }
+        
+        return answer;
+    }
+}
