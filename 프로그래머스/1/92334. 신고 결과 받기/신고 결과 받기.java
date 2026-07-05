05be6e11dc88a0bedf8262adb08844851b271505
sllip <heu0303@naver.com>
2026-07-05 22:44:31 +0900

    [level 1] Title: 신고 결과 받기, Time: 227.73 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,29 +4,26 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         HashMap<String,List<String>> map = new HashMap<>(id_list.length);
-        HashMap<String,Integer> count = new HashMap<>(id_list.length);
+        HashMap<String,Integer> user = new HashMap<>(id_list.length);
         
-        for (String id : id_list) {
-            map.put(id, new ArrayList<>());
-            count.put(id, 0);
+        for (int i = 0; i < id_list.length; i++) {
+            map.put(id_list[i], new ArrayList<>());
+            user.put(id_list[i], i);
         }
         
+        //key: 신고 받은 id, value: 신고한 id
         for (int i = 0; i < report.length; i++) {
             String[] r = report[i].split(" ");
             
-            if (!map.get(r[0]).contains(r[1])) {
-                map.get(r[0]).add(r[1]);
-                count.put(r[1], count.get(r[1]) + 1);
+            if (!map.get(r[1]).contains(r[0])) {
+                map.get(r[1]).add(r[0]);
             }
         }
         
-        for (int i = 0; i < id_list.length; i++) {
-            if (count.get(id_list[i]) >= k) {
-                
-                for (int j = 0; j < map.size(); j++) {
-                    if (map.get(id_list[j]).contains(id_list[i])) {
-                        answer[j]++;
-                    }
+        for (String id : id_list) {
+            if (map.get(id).size() >= k) {
+                for (int j = 0; j < map.get(id).size(); j++) {
+                    answer[user.get(map.get(id).get(j))]++;
                 }
             }
         }
