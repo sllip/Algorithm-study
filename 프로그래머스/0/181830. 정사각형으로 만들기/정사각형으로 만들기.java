cc740b1c48277e973a28b3e181bef0f76110f848
sllip <heu0303@naver.com>
2026-07-06 17:00:44 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.29 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[][] solution(int[][] arr) {
         int n = Math.max(arr[0].length, arr.length);
-        
         int[][] answer = new int[n][n];
         
         for (int i = 0; i < arr.length; i++) {
