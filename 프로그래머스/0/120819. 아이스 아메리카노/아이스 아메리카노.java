27de5e5a02fa30d1772d74658cc6a06eecf03a72
sllip <heu0303@naver.com>
2026-07-07 14:12:39 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,10 +1,6 @@
 class Solution {
     public int[] solution(int money) {
-        int[] answer = new int[2];
-        
-        answer[0] = money / 5500;
-        answer[1] = money - (answer[0] * 5500);
-        
+        int[] answer = new int[] {money/5500, money%5500};
         return answer;
     }
 }
