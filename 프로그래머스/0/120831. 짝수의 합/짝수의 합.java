5327e58032d68b6929e37ae67bc8a2b147d760ca
sllip <heu0303@naver.com>
2026-07-11 13:45:43 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for (int i = 1; i <= n; i++) {
-            answer += (i % 2 == 0) ? i : 0;
+        for (int i = 2; i <= n; i+=2) {
+            answer += i;
         }
         
         return answer;
