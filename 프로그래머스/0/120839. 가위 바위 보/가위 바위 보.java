492c4df693752e704024d6a1eb4bc7df1ad53071
sllip <heu0303@naver.com>
2026-07-12 16:09:40 +0900

    [level 0] Title: 가위 바위 보, Time: 3.16 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for (int i = 0; i < rsp.length(); i++) {
+            if (String.valueOf(rsp.charAt(i)).equals("0")) {
+                answer += '5';
+            } else if (String.valueOf(rsp.charAt(i)).equals("2")) {
+                answer += '0';
+            } else {
+                answer += '2';
+            }
+        }
+        
+        return answer;
+    }
+}
