b0ec230e269dd3934dc9bee4a95a3b02186b3b36
sllip <heu0303@naver.com>
2026-07-14 14:02:46 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.04 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        int s = 0, e = n-1;
+        int num = 1;
+        
+        while (num <= n*n) {
+            for (int j = s; j <= e; j++) {
+                answer[s][j] = num++;
+            }
+            
+            for (int i = s+1; i <= e; i++) {
+                answer[i][e] = num++;
+            }
+            
+            if (s == e) break;
+            
+            for (int j = e-1; j >= s; j--) {
+                answer[e][j] = num++;
+            }
+            
+            for (int i = e-1; i > s; i--) {
+                answer[i][s] = num++;
+            }
+            
+            s++; e--;
+        }
+        
+        return answer;
+    }
+}
