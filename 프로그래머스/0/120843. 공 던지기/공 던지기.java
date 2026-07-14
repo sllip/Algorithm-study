b542e5709c0093c831603bb1b62e029466cbbb74
sllip <heu0303@naver.com>
2026-07-14 16:41:57 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,14 +1,5 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        
-        switch (k*2 % numbers.length) {
-            case 0:
-                return numbers.length - 1;
-            case 1:
-                return numbers.length;
-            default:
-                return (k*2 % numbers.length)-1;       
-        }
-        
+        return (k-1)*2 % numbers.length+1;
     }
 }
