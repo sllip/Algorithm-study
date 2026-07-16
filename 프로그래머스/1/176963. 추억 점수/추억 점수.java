80c02046c9c9a778c583189f3ac7198e593b89e5
sllip <heu0303@naver.com>
2026-07-16 15:08:23 +0900

    [level 1] Title: 추억 점수, Time: 1.27 ms, Memory: 97.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<>(name.length);
+        
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        int idx = 0;
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                answer[idx] += map.get(photo[i][j]) == null ? 0 : map.get(photo[i][j]);
+            }
+            idx++;
+        }
+        
+        return answer;
+    }
+}
