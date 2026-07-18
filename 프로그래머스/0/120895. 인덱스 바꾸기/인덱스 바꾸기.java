1acd2587104eb2eaccd9f074439aecea52cdbc87
sllip <heu0303@naver.com>
2026-07-18 12:53:21 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 5.38 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char c = my_string.charAt(num1);
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (i == num1) {
+                answer += my_string.charAt(num2);
+            } else if (i == num2) {
+                answer += c;
+            } else {
+                answer += my_string.charAt(i);
+            }
+        }
+        
+        return answer;
+    }
+}
