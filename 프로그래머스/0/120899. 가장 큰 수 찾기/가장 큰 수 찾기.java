048a90fba8b2e6934999fe5f3c1540e066339b2d
sllip <heu0303@naver.com>
2026-07-18 15:40:08 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,17 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] array) {
-        HashMap<Integer, Integer> map = new HashMap<>(array.length);
+        int[] answer = new int[2];
         
         for (int i = 0; i < array.length; i++) {
-            map.put(array[i], i);
+            if (array[i] > answer[0]) {
+                answer[0] = array[i];
+                answer[1] = i;
+            }
         }
-        
-        Arrays.sort(array);
-        
-        int max = array[array.length-1];
-        int[] answer = new int[]{max, map.get(max)};
         
         return answer;
     }
