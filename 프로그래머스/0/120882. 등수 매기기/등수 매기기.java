bd489e21697ea27b9d24e108fc66250d72a523cc
sllip <heu0303@naver.com>
2026-07-24 17:40:35 +0900

    [level 0] Title: 등수 매기기, Time: 0.03 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -1,17 +1,13 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[][] score) {
         int len = score.length;
-        
         int[] answer = new int[len];
         int[] avg = new int[len];
         
         for (int i = 0; i < len; i++) {
             avg[i] = score[i][0]+score[i][1];
+            answer[i]++; //1로 초기화
         }
-        
-        Arrays.fill(answer, 1);
         
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len; j++) {
