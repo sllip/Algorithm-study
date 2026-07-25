1937a5e1124965491003cd948d150b007ad33bdf
sllip <heu0303@naver.com>
2026-07-25 18:58:12 +0900

    [level 0] Title: 특이한 정렬, Time: 0.60 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -3,17 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numlist, int n) {
         int[] answer = new int[numlist.length];
-        HashMap<Integer, Integer> map = new HashMap<>(numlist.length);
+        ArrayList<Integer> list = new ArrayList<>();
         
         for (int num : numlist) {
-            map.put(n-num, Math.abs(n-num));
+            list.add(Math.abs(n-num));
         }
         
-        ArrayList<Integer> list = new ArrayList<>(map.values());
         Collections.sort(list);
         
         for (int num : numlist) {
-            int idx = list.indexOf(map.get(n-num));
+            int idx = list.indexOf(Math.abs(n-num));
             if (answer[idx] == 0) answer[idx] = num;
             else {
                 answer[idx+1] = n-num < 0 ? answer[idx] : num;
