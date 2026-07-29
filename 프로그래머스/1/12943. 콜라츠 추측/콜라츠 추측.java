15cac399793f20e7e953c068c7a6af481fd94c80
sllip <heu0303@naver.com>
2026-07-29 12:54:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        if (num == 1) return answer;
+        
+        while (num != 1) {
+            num = num % 2 == 0 ? num/2 : num*3 + 1;
+            answer++;
+            
+            if (answer == 500) {
+                answer = -1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
