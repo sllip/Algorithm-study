473664a70e83b39c9a868d94c0f25f5714706ddc
sllip <heu0303@naver.com>
2026-08-02 13:13:56 +0900

    [level 1] Title: 기사단원의 무기, Time: 7840.80 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        for (int i = 1; i <= number; i++) {
+            int cnt = 0;
+            
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) cnt++;
+            }
+            
+            answer += cnt <= limit ? cnt : power;
+        }
+        
+        return answer;
+    }
+}
