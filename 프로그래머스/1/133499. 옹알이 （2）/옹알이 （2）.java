85c24f5510f0b8b42fb5a7c4feb88de4556b0773
sllip <heu0303@naver.com>
2026-08-03 17:24:02 +0900

    [level 1] Title: 옹알이 (2), Time: 9.58 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] words = {"aya", "ye", "woo", "ma"};
+        
+        for (String b : babbling) {
+            boolean flag = true;
+            
+            for (String w : words) {
+                if (b != b.replace(w+w, w)) flag = false;
+            }
+
+            if (flag == false) continue;
+            if (b.replaceAll("(aya|ye|woo|ma)", "").length() == 0) answer++;
+        }
+        
+        return answer;
+    }
+}
