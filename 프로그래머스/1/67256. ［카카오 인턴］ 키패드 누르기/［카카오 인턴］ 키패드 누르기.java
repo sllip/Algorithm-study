466399413c911718011b7f903042147aea333b21
sllip <heu0303@naver.com>
2026-08-11 22:36:25 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 2.75 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        int l = 10, r = 10;
+        
+        for (int n : numbers) {
+            if (n == 0) n = 11;
+            if (n%3 == 1) {
+                answer += "L";
+                l = n+2;
+            } else if (n%3 == 0) {
+                answer += "R";
+                r = n;
+            } else {
+                int dl = Math.abs(l-n);
+                int dr = Math.abs(r-n);
+                
+                if (dl%3 == 0) dl /= 3;
+                else if (dl == 5) dl = 3;
+                else if (dl > 2) dl /= 2;
+                
+                if (dr%3 == 0) dr /= 3;
+                else if (dr == 5) dr = 3;
+                else if (dr > 2) dr /= 2;
+                
+                if (dl < dr || (dl == dr && hand.equals("left"))) {
+                    answer += "L";
+                    l = n;
+                } else {
+                    answer += "R";
+                    r = n;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
